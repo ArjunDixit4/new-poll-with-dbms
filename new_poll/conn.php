8aebf831286bedda625e1911c6bedbedfa1dda86
ArjunDixit4 <85876112+ArjunDixit4@users.noreply.github.com>
2021-08-30 15:05:22 +0530

    Add files via upload

---
+++ new_poll/conn.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+  
+     $user = 'root';
+     $pwd = '';
+     $host = 'localhost';
+     $sys_dbname = 'poll';
+     $con = mysqli_connect($host,$user,$pwd,$sys_dbname) or die ;
+
+
+
+?>
